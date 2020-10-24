41cd2edd2f483ac797f9f117e637e6db6d4692e6
Ajini Perera <sahejanaperera@students.nsbm.lk>
2020-10-24 16:57:46 +0530

    test2

--- src/hotel/wiring/HotelWiring.java
+++ src/hotel/wiring/HotelWiring.java
@@ -17,44 +17,62 @@ public class HotelWiring {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        int tc = 0, m = 0, n = 0, k = 0;
-        int f =0, maxrooms = 0;
+       
+        int tc = 0;
+        int maxrooms = 0;
         
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
             if (a >= 1 && a <= 20){
                 tc = a;
             }
-        int b = in.nextInt();           //no of floors
-        if (b >= 1 && b <= 1000000){
-                m = b;
-            }
-        int c = in.nextInt();           //no of rooms
-        if (c >= 1 && c <= 1000000){
-                n = c;
-            }
-        int d = in.nextInt();           //no of turning off MS
-        if (d >= 1 && d <= m){
-                k = d;
-            }
-        
-        System.out.println("tc: "+tc+"  "+"m: "+m+"  "+"n: "+n+"  "+"k: "+k);
         
         for (int i = 0; i < tc; i++) {
+            int m = 0, n = 0, k = 0;
+            int b = in.nextInt();           //no of floors
+            if (b >= 1 && b <= 1000000){
+                    m = b;
+                }
+            int c = in.nextInt();           //no of rooms
+            if (c >= 1 && c <= 1000000){
+                    n = c;
+                }
+            int d = in.nextInt();           //no of turning off MS
+            if (d >= 1 && d <= m){
+                    k = d;
+            }
+            System.out.println("m: "+m+"  "+"n: "+n+"  "+"k: "+k);
+        
             for (int j = k; j <= m; j++) {
-                f = in.nextInt();
-                if (j == 1) {
-                    if (f == 0) {
+                int f = in.nextInt();
+                int g = in.nextInt();
+                
+                if (j==1) {
+                    if (f==m || f==0) {
+                        maxrooms = n;
+                    }
+                    if (g==m || g==0) {
                         maxrooms += n;
                     }
-                    else{
+                System.out.println("maxrooms: "+maxrooms);
+                break;
+                }
+                
+                if (j==2) {
+                    if (f==m || f==0) {
+                        maxrooms = n;
+                    }
+                    if (g==m || g==0) {
                         maxrooms += n;
                     }
+                    else
+                        maxrooms++;
+                    
+                System.out.println("maxrooms: "+maxrooms);
+                break;
                 }
             }
         }
-        System.out.println("max rooms on: " + maxrooms);
     }
     
 }
