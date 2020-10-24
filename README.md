e124c5d388f987f8676b99e23faa595bb037bf9e
Ajini Perera <sahejanaperera@students.nsbm.lk>
2020-10-24 15:38:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hotel-Wiring
+ IEEEExtreme14
---
+++ src/hotel/wiring/HotelWiring.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.wiring;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ajini Sahejana
+ */
+public class HotelWiring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int tc = 0, m = 0, n = 0, k = 0;
+        int f =0, maxrooms = 0;
+        
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+            if (a >= 1 && a <= 20){
+                tc = a;
+            }
+        int b = in.nextInt();           //no of floors
+        if (b >= 1 && b <= 1000000){
+                m = b;
+            }
+        int c = in.nextInt();           //no of rooms
+        if (c >= 1 && c <= 1000000){
+                n = c;
+            }
+        int d = in.nextInt();           //no of turning off MS
+        if (d >= 1 && d <= m){
+                k = d;
+            }
+        
+        System.out.println("tc: "+tc+"\n"+"m: "+m+"\n"+"n: "+n+"\n"+"k: "+k);
+        
+        for (int i = 0; i < tc; i++) {
+            for (int j = k; j < m; j++) {
+                f = in.nextInt();
+                if (j == 1) {
+                    if (f == 0) {
+                        maxrooms += n;
+                    }
+                    else{
+                        maxrooms += n;
+                    }
+                }
+            }
+            
+        }
+        System.out.println("max rooms on " + maxrooms);
+    }
+    
+}
